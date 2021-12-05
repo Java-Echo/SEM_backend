4acdbf92b67ff190da6c06bd6d4ee3330f26ae2a
Java-Echo <2680715897@qq.com>
2021-12-05 21:53:34 +0800

    dd

---
+++ SEM_backend1/src/test/java/com/example/sem_backend1/SemBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem_backend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
