1814a1bed419b5da49444568f7b24e4f9ea0c6ac
Java-Echo <2680715897@qq.com>
2021-12-28 16:52:01 +0800

    add middleware cors to handle the cross domain problem

--- backend/hello.go
+++ backend/hello.go
@@ -10,20 +10,20 @@ import (
 )
 
 type User struct {
-	ID string `json:"ID"`
-	Pwd string `json:"pwd"`
+	ID          string `json:"ID"`
+	Pwd         string `json:"pwd"`
 	Information string `json:"information"`
-	Identity string `json:"identity""`
+	Identity    string `json:"identity""`
 }
 type Data struct {
-	ID   int32 `json:"ID"`
-	Name string `json:"name"`
+	ID      int32  `json:"ID"`
+	Name    string `json:"name"`
 	Explain string `json:"explain"`
 }
 type Review struct {
-	ID int32 `json:"ID"`
-	Name string `json:"name"`
-	Type string `json:"type"`
+	ID       int32  `json:"ID"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
 	Revision string `json:"revision"`
 }
 
@@ -31,16 +31,17 @@ func main() {
 	//实例化echo对象。
 	e := echo.New()
 
-	db, err := sql.Open("mysql","root:200110@tcp(121.40.54.219:3306)/Search System")
+	db, err := sql.Open("mysql", "root:200110@tcp(121.40.54.219:3306)/Search System")
 	fmt.Println(err)
 	// Register account
-	e.POST("/register",func(c echo.Context) error{
+	e.POST("/register", func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		ID := c.FormValue("ID")
 		Pwd := c.FormValue("pwd")
 		Information := c.FormValue("information")
 		Identity := c.FormValue("identity")
 		// id is primary key
-		rows, err := db.Query("select ID from user where ID=?",ID)
+		rows, err := db.Query("select ID from user where ID=?", ID)
 		fmt.Println(err)
 		for rows.Next() {
 			var id string
@@ -48,19 +49,20 @@ func main() {
 				ret := User{
 					ID: "null",
 				}
-				return c.JSON(http.StatusOK,ret)
+				return c.JSON(http.StatusOK, ret)
 			}
 		}
 		stmt, err := db.Prepare("insert user set ID=?, pwd=?, information=?, identity=?")
-		res, err := stmt.Exec(ID,Pwd,Information,Identity)
+		res, err := stmt.Exec(ID, Pwd, Information, Identity)
 		ret, err := res.LastInsertId()
 		return c.JSON(http.StatusOK, ret)
 	})
 	// Login
-	e.POST("/login",func(c echo.Context) error{
+	e.POST("/login", func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		ID := c.FormValue("ID")
 		Pwd := c.FormValue("pwd")
-		rows, err := db.Query("select pwd from user where ID=?",ID)
+		rows, err := db.Query("select pwd from user where ID=?", ID)
 		fmt.Println(err)
 		for rows.Next() {
 			var passwd string
@@ -69,7 +71,7 @@ func main() {
 				// wrong passwd
 				if passwd != Pwd {
 					ret := User{
-						ID: ID,
+						ID:  ID,
 						Pwd: "null",
 					}
 					return c.JSON(http.StatusOK, ret)
@@ -90,87 +92,89 @@ func main() {
 		return c.JSON(http.StatusOK, ret)
 	})
 	// alter personal information
-	e.POST("/Person/alter", func(c echo.Context) error{
+	e.POST("/Person/alter", func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		ID := c.FormValue("ID")
 		Pwd := c.FormValue("pwd")
 		Info := c.FormValue("information")
 		stmt, err := db.Prepare("update user set pwd=?, information=? where ID =?")
 		fmt.Println(err)
-		res, err := stmt.Exec(Pwd,Info,ID)
+		res, err := stmt.Exec(Pwd, Info, ID)
 		ret, err := res.LastInsertId()
 		return c.JSON(http.StatusOK, ret)
 	})
 	// revision manager
-	e.POST("/Manager/Review", func(c echo.Context) error{
+	e.POST("/Manager/Review", func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		ID := c.FormValue("ID")
 		Type := c.FormValue("type")
 		Name := c.FormValue("name")
 		Revision := c.FormValue("revision")
 		if Type == "alter" { // alter
-			rows, err := db.Query("select ID from user where ID=?",ID)
+			rows, err := db.Query("select ID from user where ID=?", ID)
 			fmt.Println(err)
-			for rows.Next(){
+			for rows.Next() {
 				var tmp int32
-				if err := rows.Scan(&tmp); err != nil{ // not founded
+				if err := rows.Scan(&tmp); err != nil { // not founded
 					ret := Review{
-						Name : "null",
+						Name: "null",
 					}
 					return c.JSON(http.StatusOK, ret)
 				}
 			}
 			stmt, err := db.Prepare("update data set  name=?, explain=? where ID=? ")
-			res, err := stmt.Exec(Name,Revision,ID)
+			res, err := stmt.Exec(Name, Revision, ID)
 			ret, err := res.LastInsertId()
 			return c.JSON(http.StatusOK, ret)
 
-		} else if Type == "insert" {//insert
-			rows, err := db.Query("select ID from user where ID=?",ID)
+		} else if Type == "insert" { //insert
+			rows, err := db.Query("select ID from user where ID=?", ID)
 			fmt.Println(err)
-			for rows.Next(){
+			for rows.Next() {
 				var tmp int32
-				if err := rows.Scan(&tmp); err == nil{ // primary key has been used
+				if err := rows.Scan(&tmp); err == nil { // primary key has been used
 					ret := Review{
-						Name : "null",
+						Name: "null",
 					}
 					return c.JSON(http.StatusOK, ret)
 				}
 			}
 			stmt, err := db.Prepare("insert data set ID=?, name=?, explain=?")
-			res, err := stmt.Exec(ID,Name,Revision)
+			res, err := stmt.Exec(ID, Name, Revision)
 			ret, err := res.LastInsertId()
 			return c.JSON(http.StatusOK, ret)
 		} else {
 			ret := Data{
-				Name : "null",
+				Name: "null",
 			}
 			return c.JSON(http.StatusOK, ret)
 		}
 	})
 
 	//search data
-	e.POST("/search", func(c echo.Context) error{
+	e.POST("/search", func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		Name := c.FormValue("name")
 		rows, err := db.Query("select * from data where name=?", Name)
 		fmt.Println(err)
 		for rows.Next() {
 			var Name, Explain string
 			var ID int32
-			if err := rows.Scan(&ID, &Name, &Explain); err == nil{ // founded
+			if err := rows.Scan(&ID, &Name, &Explain); err == nil { // founded
 				data := Data{
-					ID : ID,
-					Name : Name,
-					Explain : Explain,
+					ID:      ID,
+					Name:    Name,
+					Explain: Explain,
 				}
 				return c.JSON(http.StatusOK, data)
 			}
 		}
 		// not founded
 		data := Data{
-			Name : "null",
+			Name: "null",
 		}
 		return c.JSON(http.StatusOK, data)
 	})
 	// start
 	e.Start(":8080")
 }
-
